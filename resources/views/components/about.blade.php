7d7066869bed5c27f3690ed21ad16932c687a33a
James <jamesballsemail@gmail.com>
2022-08-03 14:49:10 +0100

    css file

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -6,3 +6,20 @@ feel more optimistic. Our contents cover different topics, ranging from
 <a href="/animals">animals</a>{{--, <a href="#">nature</a>, <a href="#">garden</a>,
 <a href="#">home decor</a>--}}, <a href="/zodiacs">zodiac</a> and <a href="/">more</a>!
 </p>
+
+{{-- <hr>
+
+<p class="center">Contact us: wip@everything-daily.com</p>
+
+<hr>
+
+<p class="center">
+<a href="#">About US</a>
+<a href="#">Contact US</a>
+<a href="#">Cookie Policy</a>
+</p>
+<p class="center">
+<a href="#">Cookie Policy</a>
+<a href="#">DMCA</a>
+<a href="#">Terms of Service</a>
+</p> --}}
