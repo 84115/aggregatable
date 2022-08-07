8ef86e00864846a95ce18128801e9abe41e87a44
James <jamesballsemail@gmail.com>
2022-08-07 20:34:28 +0100

    latest

--- app/Console/Commands/GenerateArticleFromSiteAubtuDotbiz.php
+++ app/Console/Commands/GenerateArticleFromSiteAubtuDotbiz.php
@@ -66,7 +66,7 @@ class GenerateArticleFromSiteAubtuDotbiz extends Command
             'https://aubtu.biz/12862/',
             'https://aubtu.biz/8675/',
             'https://aubtu.biz/3813/', // NOT YET POSTED // https://aubtu.biz/3813/
-            // NOT YET POSTED // https://aubtu.biz/25499/
+            'https://aubtu.biz/25499/', // NOT YET POSTED // https://aubtu.biz/25499/
         ];
 
         $urls = [ $urls[count($urls) - 1] ];
